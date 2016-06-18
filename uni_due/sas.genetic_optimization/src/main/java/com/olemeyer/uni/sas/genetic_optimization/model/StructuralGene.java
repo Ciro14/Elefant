ddb457548c13c91de80d3e252835272606633303
Ole <meyer.ole@outlook.com>
2016-06-18 17:58:18 +0200

    add getter/setter for structural genes

--- uni_due/sas.genetic_optimization/src/main/java/com/olemeyer/uni/sas/genetic_optimization/model/StructuralGene.java
+++ uni_due/sas.genetic_optimization/src/main/java/com/olemeyer/uni/sas/genetic_optimization/model/StructuralGene.java
@@ -21,4 +21,20 @@ public class StructuralGene implements Gene {
         this.feature = feature;
         this.active= true;
     }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Feature getFeature() {
+        return feature;
+    }
+
+    public void setFeature(Feature feature) {
+        this.feature = feature;
+    }
 }
