b7fd1a3fd5ccba54e68077231d8e94727331331b
Ole <meyer.ole@outlook.com>
2016-06-18 17:58:44 +0200

    create regulatory and structural genes

--- uni_due/sas.genetic_optimization/src/main/java/com/olemeyer/uni/sas/genetic_optimization/execution/IndividualGenerator.java
+++ uni_due/sas.genetic_optimization/src/main/java/com/olemeyer/uni/sas/genetic_optimization/execution/IndividualGenerator.java
@@ -1,11 +1,18 @@
 package com.olemeyer.uni.sas.genetic_optimization.execution;
 
 import com.olemeyer.uni.sas.genetic_optimization.model.Individual;
+import com.olemeyer.uni.sas.genetic_optimization.model.RegulatoryGene;
+import com.olemeyer.uni.sas.genetic_optimization.model.StructuralGene;
 import com.olemeyer.uni.sas.genetic_optimization.utils.Utils;
 import de.uni_due.paluno.elefant.featuremodel_extended.Feature;
+import de.uni_due.paluno.elefant.featuremodel_extended.Softgoal;
+import de.uni_due.paluno.elefant.featuremodel_extended.SoftgoalImpact;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author
@@ -13,10 +20,43 @@ import java.util.List;
 @Component
 public class IndividualGenerator {
 
+    /**
+     * This method create a list of individuals based on a feature model.
+     * @param model
+     * @param count The size of the generated list.
+     * @return
+     */
     public List<Individual> generate(Feature model, int count){
+        //Get a flat list of all softgoals for creating the regulatory genes
+        List<Softgoal> allSoftgoals=Utils.getAllSoftgoals(model);
+        //Get a flat list of all features for creating the structural genes
         List<Feature> allFeatures= Utils.getAllFeatures(model);
 
+        //Create the individuals
+        List<Individual> individuals=new LinkedList<>();
+        for(int i=0;i<count;++i){
+            //Create regulatory genes
+            List<RegulatoryGene> regulatoryGenes=new LinkedList<>();
+            for(Softgoal softgoal:allSoftgoals){
+                regulatoryGenes.add(new RegulatoryGene(softgoal));
+            }
+            //Create structural genes
+            List<StructuralGene> structuralGenes=new LinkedList<>();
+            for(Feature feature:allFeatures){
+                structuralGenes.add(new StructuralGene(feature));
+            }
+            //Create connections between the regulatory and structural genes
+            Map<String,RegulatoryGene> softgoalToRegulatoryGeneMap=new HashMap<>();
+            for(RegulatoryGene rg:regulatoryGenes)softgoalToRegulatoryGeneMap.put(rg.getSoftgoal().getUuid(),rg);
+            for(StructuralGene sg:structuralGenes){
+                for(SoftgoalImpact sgimpact:sg.getFeature().getSoftgoalImpacts()){
+                    
+                }
+            }
+
+        }
         return null;
     }
+
 
 }
--- uni_due/sas.genetic_optimization/src/main/java/com/olemeyer/uni/sas/genetic_optimization/utils/Utils.java
+++ uni_due/sas.genetic_optimization/src/main/java/com/olemeyer/uni/sas/genetic_optimization/utils/Utils.java
@@ -2,20 +2,44 @@ package com.olemeyer.uni.sas.genetic_optimization.utils;
 
 import de.uni_due.paluno.elefant.featuremodel_extended.Feature;
 import de.uni_due.paluno.elefant.featuremodel_extended.FeatureConnection;
+import de.uni_due.paluno.elefant.featuremodel_extended.Softgoal;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Ole Meyer
  */
 public class Utils {
-    public static List<Feature> getAllFeatures(Feature feature){
+
+    /**
+     * This method creates a flat list of all features in the model
+     * @param model
+     * @return
+     */
+    public static List<Feature> getAllFeatures(Feature model){
         List<Feature> features=new LinkedList<>();
-        features.add(feature);
-        for(FeatureConnection featureConnection:feature.getFeatureConnections()){
-            for(Feature f:featureConnection.getFeatures())features.addAll(getAllFeatures(f));
+        features.add(model);
+        for(FeatureConnection featureConnection:model.getFeatureConnections()){
+            for(Feature feature:featureConnection.getFeatures())features.addAll(getAllFeatures(feature));
         }
         return features;
+    }
+
+    /**
+     * This method creates a list of all softgoals in the model. Normally, all softgoals should be added to the root element,
+     * but for error prevention this method will nevertheless iterate through all subfeatures
+     * @param model
+     * @return
+     */
+    public static List<Softgoal> getAllSoftgoals(Feature model){
+        List<Softgoal> softgoals=new LinkedList<>();
+        softgoals.addAll(model.getSoftgoals());
+        for(FeatureConnection featureConnection:model.getFeatureConnections()){
+            for(Feature feature:featureConnection.getFeatures())softgoals.addAll(getAllSoftgoals(feature));
+        }
+        return softgoals;
     }
 }
